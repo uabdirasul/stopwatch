9d377f523c8937dfebcb884dd506eea4ff90b77c
uabdirasulm <ubbiniyazov04@gmail.com>
2023-04-28 23:00:21 +0500

    add react state datas and functional buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,47 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hour: 0,
+      minute: 0,
+      second: 0,
+      btnDisabled: false,
+    };
+  }
+
+  startClicked = () => {
+    this.setState({
+      btnDisabled: true,
+    });
+    setInterval(() => {
+      const { hour, minute, second } = this.state;
+
+      if (second === 59) {
+        if (minute === 59) {
+          this.setState({
+            second: 0,
+            minute: 0,
+            hour: hour + 1,
+          });
+        } else {
+          this.setState({
+            second: 0,
+            minute: minute + 1,
+          });
+        }
+      } else {
+        this.setState({
+          second: second + 1,
+        });
+      }
+    }, 1000);
+  };
+
   render() {
+    const { hour, minute, second, btnDisabled } = this.state;
+
     return (
       <div>
         <div className="timer-container">
@@ -10,24 +50,30 @@ class App extends Component {
           </h1>
 
           <div className="timer-col">
-            <p className="timer-hours">0</p>
+            <p className="timer-hours">{hour}</p>
             <p className="timer-label">Hours</p>
           </div>
 
           <div className="timer-col">
-            <p className="timer-minutes">0</p>
+            <p className="timer-minutes">{minute}</p>
             <p className="timer-label">Minutes</p>
           </div>
 
           <div className="timer-col">
-            <p className="timer-seconds">0</p>
-            <p className="timer-label">Second</p>
+            <p className="timer-seconds">{second}</p>
+            <p className="timer-label">Seconds</p>
           </div>
         </div>
 
         <div className="timer-container text-center">
           <div className="timer-btn">
-            <button className="btn btn-success">Start</button>
+            <button
+              disabled={btnDisabled}
+              className="btn btn-success"
+              onClick={this.startClicked}
+            >
+              Start
+            </button>
           </div>
           <div className="timer-btn">
             <button className="btn btn-danger">Stop</button>
